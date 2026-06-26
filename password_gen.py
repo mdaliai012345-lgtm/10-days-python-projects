848c82cda62d8a70acb83a160b19a995c60f5d4a
mdaliai012345-lgtm <mdali.ai012345@gmail.com>
2026-06-26 09:34:12 +0530

    comments

--- password_gen.py
+++ password_gen.py
@@ -1,24 +1,28 @@
+# i create password generator program 
+
 import random
 pass_length = int(input("Enter the lenth of password : "))
 upper = input("incude upper letter? (yes/no) : ").lower()
-lower = input("include lower letter? (yes/no) : ").lower()
+lower = input("include lower letter? (yes/no) : ").lower()        # taking input from user 
 num   = input("include number? (yes/no) : ").lower()
 sym   = input("include symbol? (yes/no) : ").lower()
+
 char = ""
 if upper == "yes":
     char += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 if lower == "yes":
     char += "abcdefghijklmnopqrstuvwxyz"
-if num == "yes":
+if num == "yes":                                             # checking condition 
     char += "1234567890"
 if sym == "yes":
     char += "!@#$%^&*()_+"
+
 if len(char) == 0:
     print("Please select at least one option")
 else:
-
     password = ""
     for i in range (pass_length):
-        password += random.choice(char)
+        password += random.choice(char)                    # create loop for password and choose randomly
     print(password)
+
     print(len(password))
